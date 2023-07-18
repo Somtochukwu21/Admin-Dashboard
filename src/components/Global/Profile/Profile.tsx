c8956d6f946c44b159f63ab58d3d4266970c05cd
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-07-18 23:49:18 +0100

    Added a mobile menu overlay

--- src/components/Global/Profile/Profile.tsx
+++ src/components/Global/Profile/Profile.tsx
@@ -1,16 +1,16 @@
+import { Avatar } from "@mui/material";
+import { blue } from "@mui/material/colors";
 import { ReactNode } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
-import userProfile from "../../../assets/image/avatar.png";
 type ProfileType = React.ComponentProps<"div"> & { icon?: ReactNode };
 
 export const Profile = ({ className, ...props }: ProfileType) => {
   return (
     <div className="flex space-x-2">
-      <img
-        src={userProfile}
-        className={`h-[40px] w-[40px] rounded-full object-contain ${className} ${props}`}
-        alt="profile"
-      />
+      <Avatar sx={{ bgcolor: blue[200] }} alt="Admin profile pic">
+        A
+      </Avatar>
+
       <div className="flex items-center space-x-1">
         <h4 className="text-sm text-black">Administration</h4>
         <MdKeyboardArrowDown className="text-gray-500" />
